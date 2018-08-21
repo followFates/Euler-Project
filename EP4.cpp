af3b80f327cbf14d21d81e654ad25ac189ec11d0
wuyunan1 <3115629644@qq.com>
2018-08-21 09:06:57 +0800

    从2520开始遍历可整除11到20内所有整数的最小正数

--- EP4.cpp
+++ EP4.cpp
@@ -1,33 +1,23 @@
 /*************************************************************************
-	> File Name: 4.cpp
-	> Author: 
-	> Mail: 
-	> Created Time: 2018年07月31日 星期二 14时54分12秒
+	> File Name: EP4.cpp
+	> Author:tiger 
+	> Mail:3115629644@qq.com 
+	> Created Time: 2018年08月21日 星期二 08时55分49秒
  ************************************************************************/
 
 
 #include <stdio.h>
 
-
-int is_palindromic(int x) {
-    int temp = x, num = 0;
-    while (x) {
-        num = num * 10 + x % 10;
-        x /= 10;
-    }
-    if (temp == num) return 1;
-    return 0;
-}
-
 int main() {
-    int ans = 0;
-    for (int i = 100; i < 1000; i++) {
-        for(int j = 100; j < 1000; j++) {
-            if (!is_palindromic(i * j)) continue;
-            if (ans < i * j) ans = i * j;
+ 
+    int n = 2520, i;
+    while (n) {
+        for (i = 11; i <= 20; i++) {
+            if (n % i != 0) break;
         }
+        if (i == 21) break;
+        n++;
     }
-    printf("%d\n", ans);
-
+    printf("%d\n", n);
     return 0;
 }
